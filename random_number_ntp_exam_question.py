44f84c9233fdf62f17af585bfd1118bb8ae03daf
Cihat Karaboğa <hestu12@gmail.com>
2022-06-30 02:04:10 +0300

    Add random number case

---
+++ random_number_ntp_exam_question.py
@@ -0,0 +1,17 @@
+'''
+Yazacaginiz bir metot ile metota parametre olarak gelen iki tam sayi arasinda rastgele bir 
+sayi donduren metotu random kutuphanesi kullanarak yaziniz. metot parametresiz calistiginda 
+ise 0-1 arasi float sayi dondurmeli
+'''
+
+import random
+
+
+def randomNumber(a=None,b=None):
+    if a is None or b is None:
+        return random.random()
+    return random.randint(a,b)
+
+print(randomNumber())
+print(randomNumber(1,5))
+
