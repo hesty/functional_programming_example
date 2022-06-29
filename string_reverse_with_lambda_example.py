1a74154d745666cc9477798064d4d3c0f55a2aea
Cihat Karaboğa <hestu12@gmail.com>
2022-06-30 00:26:44 +0300

    Fix comment err

--- string_reverse_with_lambda_example.py
+++ string_reverse_with_lambda_example.py
@@ -1,4 +1,4 @@
-#Soru: X dizisindeki kelimeler uzunlugu 3 ten buyukse yazdir degilse a yazdir
+#Soru: X dizisindeki  kelimelerin tersinin uzunlugu 3 ten buyuk olani yazdir degilse a yazdir
 a="I lve python language"
 
 print([x[::-1] if len(x)>3 else "a"  for x in a.split(' ') ])
